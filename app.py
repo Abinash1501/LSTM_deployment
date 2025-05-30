1fb5f84139ef21752e9ead6e0ff1fe9499654af3
Abinash1501 <139610263+Abinash1501@users.noreply.github.com>
2025-05-30 16:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,8 @@ def predict():
         return jsonify({"predicted_price": float(result)})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
